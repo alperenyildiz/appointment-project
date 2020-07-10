efe42a2c0598de8f1553d6e88003cb28d4f1b067
Selçuk <selcukturedi336@gmail.com>
2020-07-10 23:24:47 +0300

    Video eklendi - Tasarım değişikliği - 10 Temmuz 2020

--- app/src/main/java/com/example/appointmentproject/MainActivity.java
+++ app/src/main/java/com/example/appointmentproject/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.appointmentproject;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
+import android.widget.VideoView;
+
 import com.example.appointmentproject.adapters.ViewAdapter;
 import com.gitonway.lee.niftymodaldialogeffects.lib.Effectstype;
 import com.gitonway.lee.niftymodaldialogeffects.lib.NiftyDialogBuilder;
@@ -22,22 +26,59 @@ public class MainActivity extends AppCompatActivity {
     ViewPager view_pager;
     ScaleTouchListener.Config config = new ScaleTouchListener.Config(150, 0.85f, 0.85f);
 
+    private VideoView videoBG;
+    MediaPlayer mMediaPlayer;
+    int mCurrentVideoPosition;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("saasdfgihjıdkofghjklş");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("TEST");
+
+
+
+        // Hook up the VideoView to our UI.
+        videoBG = (VideoView) findViewById(R.id.videoView);
+        // Build your video Uri
+        Uri uri = Uri.parse("android.resource://" // First start with this,
+                + getPackageName() // then retrieve your package name,
+                + "/" // add a slash,
+                + R.raw.coffee); // and then finally add your video resource. Make sure it is stored
+        // in the raw folder.
+
+        // Set the new Uri to our VideoView
+        videoBG.setVideoURI(uri);
+        // Start the VideoView
+        videoBG.start();
+
+        // Set an OnPreparedListener for our VideoView. For more information about VideoViews,
+        // check out the Android Docs: https://developer.android.com/reference/android/widget/VideoView.html
+        videoBG.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                mMediaPlayer = mediaPlayer;
+                // We want our video to play over and over so we set looping to true.
+                mMediaPlayer.setLooping(true);
+                // We then seek to the current posistion if it has been set and play the video.
+                if (mCurrentVideoPosition != 0) {
+                    mMediaPlayer.seekTo(mCurrentVideoPosition);
+                    mMediaPlayer.start();
+                }
+            }
+        });
+
+
+
         /////////////////////////////////////////
         layout=findViewById(R.id.layout);
         tab_layout=findViewById(R.id.tab_layout);
         view_pager=findViewById(R.id.view_pager);
         ViewAdapter adapter=new ViewAdapter(getSupportFragmentManager());
-        adapter.FragmentEkle(new LoginFragment(),"GİRİŞ YAP");
-        adapter.FragmentEkle(new RegisterFragment(),"KAYDOL");
+        adapter.FragmentEkle(new LoginFragment(),"OTURUM AÇ");
+        adapter.FragmentEkle(new RegisterFragment(),"ÜCRETSİZ KAYDOL");
         view_pager.setAdapter(adapter);
         tab_layout.setViewPager(view_pager);
         ////////////////////////////////////////////
