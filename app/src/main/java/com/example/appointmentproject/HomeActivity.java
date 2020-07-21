1f0ab8045ffe3e2366488d7ffcfdb4c666a7bfe0
Selçuk <selcukturedi336@gmail.com>
2020-07-22 00:25:47 +0300

    selçuk - datapicker/spinner eklendi - 22 Temmuz 00:25

--- app/src/main/java/com/example/appointmentproject/HomeActivity.java
+++ app/src/main/java/com/example/appointmentproject/HomeActivity.java
@@ -8,16 +8,35 @@ import android.text.TextWatcher;
 import android.text.method.PasswordTransformationMethod;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
+import java.util.Calendar;
+
+import com.jaredrummler.materialspinner.MaterialSpinner;
 import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.zigis.materialtextfield.MaterialTextField;
+
+import libs.mjn.scaletouchlistener.ScaleTouchListener;
 
 
 public class HomeActivity extends AppCompatActivity{
 
     EditText search;
+
+
+
+    private static final String TAG = "HomeActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +46,9 @@ public class HomeActivity extends AppCompatActivity{
 
 
 
+
     }
 
+    ScaleTouchListener.Config config = new ScaleTouchListener.Config(150, 0.85f, 0.85f);
 
     }
